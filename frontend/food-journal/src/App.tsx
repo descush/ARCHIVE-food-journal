d9bc0bfb2172a37dc4c203a5d31daec1f094b7bb
jgreener6 <justinmgreen06@gmail.com>
2023-06-15 16:53:18 -0400

    added submission form and log

--- frontend/food-journal/src/App.tsx
+++ frontend/food-journal/src/App.tsx
@@ -1,30 +1,61 @@
-import React from "react";
-import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getFood } from "./services/FoodService";
+import FoodForm from "./components/FoodForm";
+import { FoodContext } from "./components/FoodContext";
+import { FoodEntry, FoodEntries } from "./interface/FoodEntry";
+
+
 
 function App() {
-  const [food, setFood] = useState<any[]>([]);
+  const [foodEntries, setFoodEntries] = useState<FoodEntries>({});
 
   useEffect(() => {
-    getFood().then((data: any) => setFood(data.foods));
-    
-  }, [food]);
+    const storedEntries = localStorage.getItem("foodEntries");
+    if (storedEntries) {
+      setFoodEntries(JSON.parse(storedEntries));
+    }
+  }, []);
 
-  console.log(food)
-  
+  useEffect(() => {
+    localStorage.setItem("foodEntries", JSON.stringify(foodEntries));
+  }, [foodEntries]);
+
+  const addFood = (newFood: FoodEntry) => {
+    const date = new Date().toLocaleDateString();
+    const updatedEntries: FoodEntries = {
+      ...foodEntries,
+      [date]: [...(foodEntries[date] || []), newFood]
+    };
+    setFoodEntries(updatedEntries);
+  };
 
   return (
     <div className="App">
-      {/* <ul>
-        {food?.map((getFood) => (
-          <li key={food.description}>
-           {} 
-          </li>
+      <FoodContext.Provider value={{ addFood }}>
+        <FoodForm />
+      </FoodContext.Provider>
+
+      <div className="Log">
+        {Object.entries(foodEntries).map(([date, entries]) => (
+          <div key={date}>
+            <h2>{date}</h2>
+            <ul>
+              {entries.map((item: FoodEntry, index: number) => (
+                <li key={index}>
+                  <div>Protein: {item.protein}</div>
+                  <div>Veggies: {item.veggies}</div>
+                  <div>Fat: {item.fat}</div>
+                  <div>Carbs: {item.carbs}</div>
+                  <hr />
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
-      </ul> */}
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/food-journal/src/interface/FoodEntry.tsx
@@ -0,0 +1,11 @@
+export interface FoodEntry {
+    protein: string;
+    veggies: string;
+    fat: string;
+    carbs: string;
+  }
+  
+  export interface FoodEntries {
+    [date: string]: FoodEntry[];
+  }
+  
