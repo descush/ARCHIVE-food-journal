06b3857bf2ae140ae77351191b91c216ee556d57
jgreener6 <justinmgreen06@gmail.com>
2023-06-14 20:42:57 -0400

    added api and got it loging in the console

--- frontend/food-journal/src/App.tsx
+++ frontend/food-journal/src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import './App.css';
-import { useState, useEffect } from 'react';
-import { getFood } from './services/FoodService';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { getFood } from "./services/FoodService";
 
 function App() {
-
-  const [food, setFood] = useState<any[]>([])
+  const [food, setFood] = useState<any[]>([]);
 
   useEffect(() => {
-    getFood().then((data: any) => setFood(data.results))
-  }, [])
+    getFood().then((data: any) => setFood(data.foods));
+    
+  }, [food]);
 
-
-
- 
+  console.log(food)
+  
 
   return (
     <div className="App">
+      {/* <ul>
+        {food?.map((getFood) => (
+          <li key={food.description}>
+           {} 
+          </li>
+        ))}
+      </ul> */}
     </div>
   );
 }
--- frontend/food-journal/src/services/FoodService.ts
+++ frontend/food-journal/src/services/FoodService.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "axios"
 
 export function getFood(): any {
-    return axios.get('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY')
+    return axios.get('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=UGxFWbRbmPW0QfYnbAmGna4tH017KBN6NasEtfMu')
     .then(response => response.data)
 }
