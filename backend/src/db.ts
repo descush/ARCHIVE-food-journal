f67b3eb6bc53b4a9652d5764d5989a3b5707fe9d
jgreener6 <justinmgreen06@gmail.com>
2023-06-19 21:35:03 -0400

    got the backend working for adding and calling entries and individual entries

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,13 +2,21 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log('MongoDB URI:', process.env.URI);
+
 
 const uri: string = process.env.URI || "";
 const client: MongoClient = new MongoClient(uri);
 
 export const getClient =async () => {
-
+try {
     await client.connect();
     return client;
-    
+} catch (error) {
+    console.error("error connecting")
+    throw error;
 }
+    
+};
+
+console.log("MongoDB URI:", uri);
--- backend/src/routes/entryRoutes.ts
+++ backend/src/routes/entryRoutes.ts
@@ -3,7 +3,7 @@ import { getClient } from "../db";
 import { ObjectId } from "mongodb";
 import Entry from "../models/Entry";
 
-const entryRouter = express.Router();
+export const entryRouter = express.Router();
 
 const errorResponse = (error: any, res: any) => {
     console.error("FAIL", error);
@@ -13,46 +13,51 @@ const errorResponse = (error: any, res: any) => {
   // endpoints
 
     //get all
-  entryRouter.get('/entries', async (req, res)=> {
-    const searchTerm = req.query.search ? req.query.search : "";
-    const min = req.query.min ? +req.query.min : 0;
-    console.log(searchTerm);
-    console.log(min);
-    const client = await getClient();
-    const results = await client.db().collection<Entry>("entries").find({
-      $and: [{ setUp: new RegExp(`${searchTerm}`) }, { rating: { $gte: min } }],
-    }).toArray();
-    res.status(200).json(results);
-  });
+    entryRouter.get('/entries', async (req, res) => {
+      try {
+        const client = await getClient();
+        const results = await client.db().collection<Entry>('entries').find().toArray();
+        res.status(200).json(results);
+      } catch (err) {
+        errorResponse(err, res);
+      }
+    });
+
+  // entryRouter.get('/entries', (req, res) => {
+  //   res.send('Entries endpoint is working!');
+  // });
+  
 
   //get one
   entryRouter.get('/entries/:id', async (req, res) => {
-    try{
-    const id = new ObjectId(req.params.id);
-  
-    const client = await getClient();
-    const result = await client.db().collection<Entry>("entries")
-    .findOne({_id: id});
-    
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      res.status(404).json({message:'entry not found'});
-    }
+    try {
+      const id = req.params.id;
+      if (!ObjectId.isValid(id)) {
+        res.status(400).json({ message: 'Invalid entry ID' });
+        return;
+      }
+      const client = await getClient();
+      const result = await client.db().collection<Entry>('entries').findOne({ _id: new ObjectId(id) });
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ message: 'Entry not found' });
+      }
     } catch (err) {
       errorResponse(err, res);
     }
   });
 
-
     // create one
-  entryRouter.post('/entries', async (req, res)=> {
-    try {
-    const newEntry = req.body as Entry;
-    const client = await getClient();
-    await client.db().collection<Entry>('entries').insertOne(newEntry);
-    res.status(201).json(newEntry);
-  } catch (err) {
-      errorResponse(err, res);
-    }
-  });
+    entryRouter.post('/entries', async (req, res) => {
+      try {
+        const newEntry = req.body as Entry;
+        const client = await getClient();
+        const result = await client.db().collection<Entry>('entries').insertOne(newEntry);
+        console.log(result);
+        res.status(201).json(newEntry);
+      } catch (err) {
+        console.error("Error creating entry:", err);
+        res.status(500).json({ message: "Failed to create entry" });
+      }
+    });
